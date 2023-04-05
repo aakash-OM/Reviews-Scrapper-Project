5b6f2e96605f4c239d1bb459ce47155a8e3e5578
Aakash Mittal <aakashmittal0207@gmail.com>
2023-04-05 16:39:12 +0530

    application.py is modified

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 import pickle
@@ -11,44 +11,50 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("login.html")
-database={'Aakash':'1234','Dev':'4321','Harry':'@1234'}
 
-@app.route('/form_login',methods=['POST','GET'])
+
+database = {'Aakash': '1234', 'Dev': '4321', 'Harry': '@1234'}
+
+
+@app.route('/form_login', methods=['POST', 'GET'])
 def login():
-    name1=request.form['username']
-    pwd=request.form['password']
+    name1 = request.form['username']
+    pwd = request.form['password']
     if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
+        return render_template('login.html', info='Invalid User')
     else:
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
+        if database[name1] != pwd:
+            return render_template('login.html', info='Invalid Password')
         else:
-	        return render_template('index.html',name=name1)
+            return render_template('index.html', name=name1)
 
 
-
-
-@app.route('/review',methods=['GET'])  # route to display the home page
+@app.route('/review', methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
-@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+
+
+# route to show the review comments in a web UI
+@app.route('/review', methods=['POST', 'GET'])
 @cross_origin()
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ", "")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.find_all(
+                "div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
             box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            productLink = "https://www.flipkart.com" + \
+                box.div.div.div.a['href']
             prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
+            prodRes.encoding = 'utf-8'
             prod_html = bs(prodRes.text, "html.parser")
             print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
@@ -60,45 +66,46 @@ def index():
             reviews = []
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    # name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all(
+                        'p', {'class': '_2sc7ZR _2V5EHH'})[0].text
 
                 except:
                     name = 'No Name'
 
                 try:
-                    #rating.encode(encoding='utf-8')
+                    # rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
-
 
                 except:
                     rating = 'No Rating'
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
+                    # commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
 
                 except:
                     commentHead = 'No Comment Heading'
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
+                    # custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
                 except Exception as e:
-                    print("Exception while creating dictionary: ",e)
+                    print("Exception while creating dictionary: ", e)
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
             return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
-            print('The Exception message is: ',e)
+            print('The Exception message is: ', e)
             return 'something is wrong'
     # return render_template('results.html')
 
     else:
         return render_template('index.html')
 
+
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True)
+    app.run(debug=True)
